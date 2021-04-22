8ee4876c9d3224b030924950d838c3c97fc083e1
Sandro Gerber <sandro-gerber@outlook.com>
2021-04-23 00:01:13 +0200

    Add vuetify

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,30 @@
 module.exports = {
   root: true,
+
   env: {
     node: true
   },
+
   extends: [
     'plugin:vue/essential',
     '@vue/standard',
     '@vue/typescript/recommended'
   ],
+
   parserOptions: {
-    ecmaVersion: 2020
+    ecmaVersion: 2020,
+    parser: '@typescript-eslint/parser'
   },
+
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
+  },
+
+  extends: [
+    'plugin:vue/essential',
+    '@vue/standard',
+    '@vue/typescript/recommended',
+    '@vue/typescript'
+  ]
 }
