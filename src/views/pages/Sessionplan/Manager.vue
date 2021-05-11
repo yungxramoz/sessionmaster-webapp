991b786c43faf0f8150a6c0408718bd18b2a5a51
Sandro Gerber <sandro-gerber@outlook.com>
2021-05-11 22:17:36 +0200

    Implement initial manager UI

--- src/views/pages/Sessionplan/Manager.vue
+++ src/views/pages/Sessionplan/Manager.vue
@@ -1,12 +1,40 @@
 <template>
   <v-container>
-    <h1>Manager</h1>
-    <span v-if="loading">loading</span>
-    <ul v-else>
-      <li v-for="plan in ownedSessionplans" :key="plan.Id">
-        {{ plan.name }}
-      </li>
-    </ul>
+    <template v-if="loading">
+      <yr-centered-progress-linear text="Getting your spellbook ready..." />
+    </template>
+    <v-col v-else cols="12">
+      <v-row v-for="plan in ownedSessionplans" :key="plan.Id" class="pb-2">
+        <v-card outlined elevation="2" class="mx-auto" min-width="100%">
+          <v-card-title class="text-h5">
+            {{ plan.name }}
+          </v-card-title>
+
+          <v-card-subtitle>Next Session: </v-card-subtitle>
+
+          <v-card-actions>
+            <v-spacer />
+            <yr-btn text color="error">
+              <v-icon class="mr-1">
+                mdi-delete
+              </v-icon>
+              Delete
+            </yr-btn>
+            <yr-btn text :to="'/sessionplan/details/' + plan.id">
+              <v-icon class="mr-1">
+                mdi-open-in-new
+              </v-icon>
+              Open
+            </yr-btn>
+          </v-card-actions>
+        </v-card>
+      </v-row>
+      <v-row>
+        <yr-icon-btn height="130px" fab tile block :large-icon="true" data-cy="add-sessionplan-btn">
+          mdi-plus
+        </yr-icon-btn>
+      </v-row>
+    </v-col>
   </v-container>
 </template>
 
