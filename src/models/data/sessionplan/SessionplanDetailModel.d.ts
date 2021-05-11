147a5a01321da6711b0c10f36bf0792d85844b7c
Sandro Gerber <sandro-gerber@outlook.com>
2021-05-11 18:35:11 +0200

    Update sessions type to array

--- src/models/data/sessionplan/SessionplanDetailModel.d.ts
+++ src/models/data/sessionplan/SessionplanDetailModel.d.ts
@@ -1,6 +1,6 @@
-import { SessionModel } from '@/models/data/session/SessionModel'
+import { SessionModel } from '@/models/data/session'
 import SessionplanOverviewModel from './SessionplanOverviewModel'
 
 export default interface SessionplanDetailModel extends SessionplanOverviewModel {
-  sessions: SessionModel
+  sessions: SessionModel[]
 }
